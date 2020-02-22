cecb68158e1c5877025947c471687ddadd350bdb
SweetDreems <61348190+SweetDreems@users.noreply.github.com>
2020-02-22 11:25:30 +0200

    correct

--- p.py
+++
@@ -1,8 +0,0 @@
-import os
-
-folder = 'pepsy'
-
-if os.path.exists(folder):
-    print("I'm find")
-else:
-    print("I'm not find")
--- read.py
+++
@@ -1,8 +0,0 @@
-inp = open('input.txt', 'r')
-# i need take vars in the txt file
-def tx():
-    for line in inp:    
-        print(line)
-        print(line[0])
-        
-tx()
